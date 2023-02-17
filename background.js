9a0a4b8fc005192611340f1ac742992071af47ee
이호진 <75923099+icemokacat@users.noreply.github.com>
2023-02-17 15:51:36 +0900

    임시저장

--- background.js
+++ background.js
@@ -1,12 +1,155 @@
-/**
-* ---------------------------------------------------------------------------------
-* | 백그라운드 |
-* ---------------------------------------------------------------------------------
-* - 디폴트 컬러를 지정하여 스토리지 API 를 호출하여 지정한 색을 저장시킵니다.
-**/
-let color = '#3aa757';
+/************************************************************
+ * google.com 에서 검색시 이상한 불필요한 수집용 사이트는 
+ * black list에 추가 하여 구글 검색결과에 더 이상 나오지 않도록 한다.
+ ************************************************************/
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('기본 배경색은 %cgreen', `color: ${color}`);
+//////////////////////////////////////////////
+// 기존 사용자가 option 에서 설정 한 값을 들고옴
+//////////////////////////////////////////////
+
+
+// variable define
+const extensionListName = 'blackExtensionList';
+const domainListName = 'blackDomainList';
+const blacklistName = "googleBlackList";
+const whitelistName = "googleWhiteList";
+var blackList = [];
+var whiteList = [];
+
+// black list 를 가져오는 함수
+function getBlackList() {
+    chrome.storage.sync.get(blacklistName, function (data) {
+        blackList = data.blackList;
+    });
+}
+
+// black list 를 저장하는 함수
+function setBlackList() {
+    chrome.storage.sync.set({blackList: blackList});
+}
+
+// black list 에 추가하는 함수
+function addBlackList(url) {
+    blackList.push(url);
+    setBlackList();
+}
+
+// black list 에서 제거하는 함수
+function removeBlackList(url) {
+    blackList = blackList.filter(function (item) {
+        return item !== url;
+    });
+    setBlackList();
+}
+
+// 기존 black list 인지 확인하는 함수
+function isBlackList(url) {
+    return blackList.includes(url);
+}
+
+// 지금 사이트가 구글 검색 페이지인지 확인하는 함수
+function isGoogleSearch() {
+  try{
+    if(window){
+      return window.location.href.indexOf("google.com/search") !== -1;
+    }else{
+      return false;
+    }
+  }catch(e){
+    return false;
+  }
+}
+
+// 검색된 페이지 목록을 조회
+function getSearchResult() {
+    return document.querySelectorAll("div.g");
+}
+
+function isWhiteList(url) {
+    return whiteList.includes(url);
+}
+
+// 가짜 혹은 수집용, chatgpt 가 만든 사이트인지 확인하는 함수
+function checkIsTrashSite(url) {
+
+  // 0. white list 인지 확인
+  if (isWhiteList(url)) {
+    return false;
+  }
+
+  // 1. extension list 인지 확인
+  let extension = url.split(".").pop();
+  if (isExtensionList(extension)) {
+    return true;
+  }
+
+  // 2. domain list 인지 확인
+  let domain = url.split("/")[2];
+  if (isDomainList(domain)) {
+    return true;
+  }
+
+  // 3. black list 인지 확인
+  if (isBlackList(url)) {
+    return true;
+  }
+
+  // 4. 추가로 확인
+  // 4.1 도메인 URL에서 . 으로 구분된 문자열 중 오직 숫자로만 구성된 문자열이 있으면
+  //     이상한 사이트로 판단
+  let domainArray = domain.split(".");
+  for (let i = 0; i < domainArray.length; i++) {
+    if (domainArray[i].match(/^[0-9]+$/)) {
+      return true;
+    }
+  }
+
+  // 4.2 쿠키에 chatgpt 가 있으면 이상한 사이트로 판단
+  let cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    if (cookies[i].indexOf("chatgpt") !== -1) {
+      return true;
+    }
+  }
+
+}
+
+// 검색된 페이지(리스트)에서 이상한 사이트를 찾는 함수
+function findBlackList() {
+  var findTrashCount = 0;
+    let searchResult = getSearchResult();
+    searchResult.forEach(function (item) {
+        let url = item.querySelector("a").href;
+        if (checkIsTrashSite(url)) {
+            findTrashCount++;
+            // A. 기존 black list 인지 확인
+            if (isBlackList(url)) {
+                // A-1. 기존 black list 이면 해당 검색결과를 숨김
+                item.style.display = "none";
+            } else {  
+                // A-2. 기존 black list 가 아니면 black list 추가
+                addBlackList(url);
+            }
+        }
+    });
+    // 작업이 끝나면 확인을 위해 console 에 출력
+    console.log("findTrashCount : " + findTrashCount);
+}
+
+// 검색이 완료되면 실행
+function run() {
+    if (isGoogleSearch()) {
+        findBlackList();
+    }
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    // 기존 사용자가 option 에서 설정 한 값을 들고옴
+    chrome.storage.sync.get([blacklistName, whitelistName], function (data) {
+        blackList = data.blackList;
+        whiteList = data.whiteList;
+    });
+
+    // 검색이 완료되면 실행
+    run();
 });
--- options.js
+++ options.js
@@ -4,49 +4,255 @@
 * ---------------------------------------------------------------------------------
 **/
 
-// button element 요소
-let page = document.getElementById("buttonDiv");
-// CSS 클래스명
-let selectedClassName = "current";
-// 제공할 배경색 목록
-const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+// variable define
+const extensionListName = 'blackExtensionList';
+const domainListName = 'blackDomainList';
+const blackListName = 'googleBlackList';
+const whiteListName = 'googleWhiteList';
 
-/**
- * @param {object} event - 이벤트
- * @description 스토리지 API 를 호출하여 배경색을 저장하고 현재 웹 페이지의 배경색을 변경하여 줍니다.
- **/
-function handleButtonClick(event) {
-  let current = event.target.parentElement.querySelector(`.${selectedClassName}`);
-  if (current && current !== event.target) {
-    current.classList.remove(selectedClassName);
-  }
+let blackList = [];
+let whiteList = [];
+let domainList = [];
+let extensionList = [];
 
-  let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
-  chrome.storage.sync.set({ color });
+// A. element define
+const extensionTextBox = document.getElementById('extension');
+const addExtensionButton = document.getElementById('addExtension');
+const extensionListBox = document.getElementById('extensionList');
+const domainTextBox = document.getElementById('domain');
+const addDomainButton = document.getElementById('addDomain');
+const domainListBox = document.getElementById('domainList');
+const blackListBox = document.getElementById('blackList');
+const whiteListBox = document.getElementById('whiteList');
+
+// C. common function define
+// C.1. element 를 추가하는 함수
+function addElement(element, text) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  element.appendChild(li);
 }
 
-/**
- * @param {object} buttonColors - 버튼 컬러 목록
- * @description 제공할 배경색을 웹 페이지에 표시하여 줍니다.
- **/
-function constructOptions(buttonColors) {
-  chrome.storage.sync.get("color", (data) => {
-    let currentColor = data.color;
-    for (let buttonColor of buttonColors) {
-      let button = document.createElement("button");
-      button.dataset.color = buttonColor;
-      button.style.backgroundColor = buttonColor;
+// C.2. element 를 제거하는 함수
+function removeElement(element, text) {
+  const li = element.querySelector('li');
+  if (li && li.textContent === text) {
+    element.removeChild(li);
+  }
+}
 
-      if (buttonColor === currentColor) {
-        button.classList.add(selectedClassName);
+// C.3. chrome.storage 기능
+// C.3.1 chrome.storage 에서 가져오는 함수
+function getStorage(key, callback) {
+  chrome.storage.sync.get(key, function (data) {
+    callback(data[key]);
+  });
+}
+
+// C.3.2 chrome.storage 에 저장하는 함수
+function setStorage(key, value) {
+  chrome.storage.sync.set({ [key]: value });
+}
+
+// C.4. chrome.storage 에서 가져오는 함수
+function getLocalVarFromStorage(key, listVar, callback) {
+  chrome.storage.sync.get(key, function (data) {
+    listVar = data[key];
+    callback();
+  });
+}
+
+// C.5. chrome.storage 에 저장하는 함수
+function setLocalVarToStorage(key, listVar) {
+  chrome.storage.sync.set({ [key]: listVar });
+}
+
+//////////////////////////////////////////////////////////
+
+
+// B. function define
+// B-1. extension list 를 가져오는 함수
+function getExtensionList() {
+  getStorage(extensionListName, function (data) {
+    extensionList.value = data;
+  });
+}
+
+// B-2. extension list 를 저장하는 함수
+function setExtensionList() {
+  setStorage(extensionListName, extensionList.value);
+}
+
+// B-3. extension list 에 추가하는 함수
+function addExtensionList(extension) {
+  extensionList.value += extension + ';';
+  setExtensionList();
+}
+
+// B-4. extension list 에서 제거하는 함수
+function removeExtensionList(extension) {
+  extensionList.value = extensionList.value.replace(extension + ';', '');
+  setExtensionList();
+}
+
+// B-5. extension list 인지 확인하는 함수
+function isExtensionList(extension) {
+  if(extensionList === undefined || extensionList.value === undefined){
+    return false;
+  }else{
+    return extensionList.value.indexOf(extension) !== -1;
+  }
+}
+
+// B-6. domain list 를 가져오는 함수
+function getDomainList() {
+  getStorage(domainListName, function (data) {
+    domainList.value = data;
+  });
+}
+
+// B-7. domain list 를 저장하는 함수
+function setDomainList() {
+  setStorage(domainListName, domainList.value);
+}
+
+// B-8. domain list 에 추가하는 함수
+function addDomainList(domain) {
+  domainList.value += domain + ';';
+  setDomainList();
+}
+
+// B-9. domain list 에서 제거하는 함수
+function removeDomainList(domain) {
+  domainList.value = domainList.value.replace(domain + ';', '');
+  setDomainList();
+}
+
+// B-10. domain list 인지 확인하는 함수
+function isDomainList(domain) {
+  if(domainList === undefined || domainList.value === undefined){
+    return false;
+  }else{
+    return domainList.value.indexOf(domain) !== -1;
+  }
+}
+
+// B-11. black list 를 가져오는 함수
+function getBlackList() {
+  getStorage(blackListName, function (data) {
+    blackList.value = data;
+  });
+}
+
+// B-12. black list 를 저장하는 함수
+function setBlackList() {
+  setStorage(blackListName, blackList.value);
+}
+
+// B-13. white list 를 가져오는 함수
+function getWhiteList() {
+  getStorage(whiteListName, function (data) {
+    whiteList.value = data;
+  });
+}
+
+// B-14. white list 를 저장하는 함수
+function setWhiteList() {
+  setStorage(whiteListName, whiteList.value);
+}
+
+// C. event define
+// C-1. extension list 에 추가 버튼을 클릭하였을 경우
+addExtensionButton.addEventListener('click', function () {
+  let extension = extensionTextBox.value;
+  if (extension !== '') {
+    if (!isExtensionList(extension)) {
+      addExtensionList(extension);
+    }
+    extensionTextBox.value = '';
+  }
+});
+
+
+// C-3. domain list 에 추가 버튼을 클릭하였을 경우
+addDomainButton.addEventListener('click', function () {
+  let domain = domainTextBox.value;
+  if (domain !== '') {
+    if (!isDomainList(domain)) {
+      addDomainList(domain);
+    }
+    domainTextBox.value = '';
+  }
+});
+
+// C-5. black list 를 출력하는 함수
+function printBlackList() {
+  chrome.storage.sync.get(blackListName, function (data) {
+    let blackList = data.blackList;
+    let blackListArray = blackList.split(';');
+    let blackListHtml = '';
+    for (let i = 0; i < blackListArray.length; i++) {
+      if (blackListArray[i] !== '') {
+        blackListHtml += '<li>' + blackListArray[i] + '<button>제거</button></li>';
       }
+    }
+    blackListBox.innerHTML = blackListHtml;
 
-      button.addEventListener("click", handleButtonClick);
-      page.appendChild(button);
+    // C-9. black list 에 추가 버튼을 먼저 찾아서 (blackListBox기준으로 찾음), 이벤트를 등록
+    let blackListAddButton = blackListBox.querySelectorAll('button');
+    for (let i = 0; i < blackListAddButton.length; i++) {
+      blackListAddButton[i].addEventListener('click', function () {
+        let domain = blackListAddButton[i].previousSibling.textContent;
+        if (!isDomainList(domain)) {
+          addDomainList(domain);
+        }
+        removeBlackList(domain);
+      });
     }
   });
 }
 
-// 최초 버튼 컬러 표시 및 이벤트 등록 호출
-constructOptions(presetButtonColors);
+// C-7. white list 를 출력하는 함수
+function printWhiteList() {
+  chrome.storage.sync.get(whiteListName, function (data) {
+    let whiteList = data.whiteList;
+    let whiteListArray = whiteList.split(';');
+    let whiteListHtml = '';
+    for (let i = 0; i < whiteListArray.length; i++) {
+      if (whiteListArray[i] !== '') {
+        whiteListHtml += '<li>' + whiteListArray[i] + '<button>제거</button></li>';
+      }
+    }
+    whiteListBox.innerHTML = whiteListHtml;
+
+    // C-10. white list 에 추가 버튼을 먼저 찾아서 (whiteListBox기준으로 찾음), 이벤트를 등록
+    let whiteListAddButton = whiteListBox.querySelectorAll('button');
+    for (let i = 0; i < whiteListAddButton.length; i++) {
+      whiteListAddButton[i].addEventListener('click', function () {
+        let domain = whiteListAddButton[i].previousSibling.textContent;
+        if (!isDomainList(domain)) {
+          addDomainList(domain);
+        }
+        removeWhiteList(domain);
+      });
+    }
+  });
+}
+
+// D. init
+// D-1. extension list 를 가져온다.
+getExtensionList();
+
+// D-2. domain list 를 가져온다.
+getDomainList();
+
+// D-3. black list 를 가져온다.
+getBlackList();
+
+// D-4. white list 를 가져온다.
+getWhiteList();
+
+// E. test
+// E-1. extension list 에 추가
+// addExtensionList('test');
+// ... 
